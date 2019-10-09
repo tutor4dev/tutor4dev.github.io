b2fe996aff2b10b28448d97a2af84f15dc47a6d2
Kongthap Thammachat <jeud@hotmail.com>
2019-10-09 22:58:15 +0700

    update content

---
+++ assets/js/44.bddf39c6.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[44],{198:function(t,e,n){"use strict";n.r(e);var s=n(1),r=Object(s.a)({},function(){var t=this.$createElement,e=this._self._c||t;return e("div",{staticClass:"content"},[e("HeroCourse"),this._v(" "),e("HeroArticle")],1)},[],!1,null,null,null);e.default=r.exports}}]);
